1f4fbaf1b933a081a70a6bf5a4f732a69037057d
Wendell <root@tzeentch.io>
2025-07-22 02:14:19 +0800

    fix: SizePreferenceKey reduce logic
    Changed the reduce method to sum the width and height of CGSize values instead of leaving the value unchanged. I also updated defaultValue to be a constant. This resolves a UI overlapping issue in CarouselStack.

--- Sources/Utils/PreferenceKeys/SizePreferenceKey.swift
+++ Sources/Utils/PreferenceKeys/SizePreferenceKey.swift
@@ -4,7 +4,9 @@ import SwiftUI
 public struct SizePreferenceKey: PreferenceKey {
     public typealias Value = CGSize
 
-    public static var defaultValue: Value = .zero
+    public static let defaultValue: Value = .zero
 
-    public static func reduce(value: inout Value, nextValue: () -> Value) { }
+    public static func reduce(value: inout Value, nextValue: () -> Value) {
+        value = CGSize(width: value.width + nextValue().width, height: value.height + nextValue().height)
+    }
 }
